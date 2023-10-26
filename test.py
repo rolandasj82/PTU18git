002fad2a338efa8599fb8cf5e39edc4672bb295e
Rolandas J <rolandas.jonusys@outlook.com>
2023-10-26 11:18:43 +0300

    Test failas. Pridetas dar vienas BIN2

--- test.py
+++ test.py
@@ -3,4 +3,7 @@ print(var1.bit_length())  # 5
 print(var1.bit_count())  # 2
 bin_var1 = bin(var1)
 print(bin_var1)  # 0b10010
+bin_var2=0b10111
+print(bin_var2)  # 23
+print(bin(bin_var2))  # 0b10111
 
